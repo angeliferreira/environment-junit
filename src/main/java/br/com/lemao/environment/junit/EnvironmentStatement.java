09d8cf18ee217caab0caab282c15081cfc9f863e
Guilherme de Angeli Ferreira <angeliferreira@gmail.com>
2020-02-21 10:57:07 +0100

    BugFix: A bug was found when class annotation and test method with multiple environments. Should run the closest configuration, first method then class.

--- src/main/java/br/com/lemao/environment/junit/EnvironmentStatement.java
+++ src/main/java/br/com/lemao/environment/junit/EnvironmentStatement.java
@@ -22,7 +22,7 @@ public class EnvironmentStatement extends Statement {
 	public EnvironmentStatement(Statement statement, Description description) {
 		this(statement, description, new EnvironmentReflectionFactory());
 	}
-	
+
 	public EnvironmentStatement(Statement statement, Description description, EnvironmentFactory environmentFactory) {
 		this.statement = statement;
 		this.description = description;
@@ -30,18 +30,28 @@ public class EnvironmentStatement extends Statement {
 	}
 
 	protected void before() {
-		GivenEnvironment givenEnvironment = getGivenEnvironmentAnnotation();
-		GivenEnvironments givenEnvironments = getAnnotation(GivenEnvironments.class);
+		validateGivenEnvironmentAndGivenEnvironmentsInTheSameClassOrMethod();
 
-		if ((givenEnvironment == null && givenEnvironments == null) || getIgnoreEnvironmentAnnotation() != null) return;
+		if ((getGivenEnvironmentAnnotation() == null && getGivenEnvironmentsAnnotation() == null)
+				|| getIgnoreEnvironmentAnnotation() != null)
+			return;
 
-		if ((getAnnotationMethod(GivenEnvironment.class) != null && getAnnotationMethod(GivenEnvironments.class) != null) ||
-		    (getAnnotationClass(GivenEnvironment.class) != null && getAnnotationClass(GivenEnvironments.class) != null)) {
-		    throw new EnvironmentException("@GivenEnvironments and @GivenEnvironment annotation in the same class or method !?");
-		} else if (givenEnvironment != null) {
-			EnvironmentExecutor.gimme(environmentFactory).execute(givenEnvironment);
-		} else if (givenEnvironments != null) {
-			EnvironmentExecutor.gimme(environmentFactory).execute(givenEnvironments);
+		if (hasAnnotationMethod()) {
+			GivenEnvironment givenEnvironmentMethod = getAnnotationMethod(GivenEnvironment.class);
+			GivenEnvironments givenEnvironmentsMethod = getAnnotationMethod(GivenEnvironments.class);
+			if (givenEnvironmentMethod != null) {
+				EnvironmentExecutor.gimme(environmentFactory).execute(givenEnvironmentMethod);
+			} else if (givenEnvironmentsMethod != null) {
+				EnvironmentExecutor.gimme(environmentFactory).execute(givenEnvironmentsMethod);
+			}
+		} else {
+			GivenEnvironment givenEnvironmentClass = getAnnotationClass(GivenEnvironment.class);
+			GivenEnvironments givenEnvironmentsClass = getAnnotationClass(GivenEnvironments.class);
+			if (givenEnvironmentClass != null) {
+				EnvironmentExecutor.gimme(environmentFactory).execute(givenEnvironmentClass);
+			} else if (givenEnvironmentsClass != null) {
+				EnvironmentExecutor.gimme(environmentFactory).execute(givenEnvironmentsClass);
+			}
 		}
 	}
 
@@ -49,6 +59,21 @@ public class EnvironmentStatement extends Statement {
 		// Nothing to do here
 	}
 
+	private void validateGivenEnvironmentAndGivenEnvironmentsInTheSameClassOrMethod() {
+		if ((getAnnotationMethod(GivenEnvironment.class) != null && getAnnotationMethod(GivenEnvironments.class) != null) ||
+				(getAnnotationClass(GivenEnvironment.class) != null && getAnnotationClass(GivenEnvironments.class) != null)) {
+			throw new EnvironmentException("@GivenEnvironments and @GivenEnvironment annotation in the same class or method !?");
+		}
+	}
+
+	private boolean hasAnnotationMethod() {
+		return getAnnotationMethod(GivenEnvironment.class) != null || getAnnotationMethod(GivenEnvironments.class) != null;
+	}
+	
+	private GivenEnvironments getGivenEnvironmentsAnnotation() {
+		return getAnnotation(GivenEnvironments.class);
+	}
+	
 	private GivenEnvironment getGivenEnvironmentAnnotation() {
 		return getAnnotation(GivenEnvironment.class);
 	}
@@ -60,18 +85,19 @@ public class EnvironmentStatement extends Statement {
 	private <T extends Annotation> T getAnnotation(Class<T> annotationType) {
 		T annotation = getAnnotationMethod(annotationType);
 
-		if (annotation != null) return annotation;
+		if (annotation != null)
+			return annotation;
 
 		return getAnnotationClass(annotationType);
 	}
 
-    private <T extends Annotation> T getAnnotationClass(Class<T> annotationType) {
-        return description.getTestClass().getAnnotation(annotationType);
-    }
+	private <T extends Annotation> T getAnnotationClass(Class<T> annotationType) {
+		return description.getTestClass().getAnnotation(annotationType);
+	}
 
-    private <T extends Annotation> T getAnnotationMethod(Class<T> annotationType) {
-        return description.getAnnotation(annotationType);
-    }
+	private <T extends Annotation> T getAnnotationMethod(Class<T> annotationType) {
+		return description.getAnnotation(annotationType);
+	}
 
 	@Override
 	public void evaluate() throws Throwable {
--- src/test/java/br/com/lemao/environment/test/EnvironmentWithClassAnnotationTest.java
+++ src/test/java/br/com/lemao/environment/test/EnvironmentWithClassAnnotationTest.java
@@ -11,9 +11,13 @@ import org.junit.Rule;
 import org.junit.Test;
 
 import br.com.lemao.environment.annotation.GivenEnvironment;
+import br.com.lemao.environment.annotation.GivenEnvironments;
 import br.com.lemao.environment.environments.BikerEnvironment;
 import br.com.lemao.environment.environments.OneMaleBikerAndOneBicycleForThisBiker;
 import br.com.lemao.environment.environments.TwoBikersOneMaleAnotherFemaleAndOneBicycleForMaleBiker;
+import br.com.lemao.environment.environments.multiple.OneFemaleBikerWithBicycleEnvironment;
+import br.com.lemao.environment.environments.multiple.OneMaleBikerWithBicycleEnvironment;
+import br.com.lemao.environment.environments.multiple.TwoBicyclesEnvironment;
 import br.com.lemao.environment.junit.InMemoryRule;
 import br.com.lemao.environment.junit.annotation.IgnoreEnvironment;
 import br.com.lemao.environment.model.bicycle.Bicycle;
@@ -27,140 +31,165 @@ public class EnvironmentWithClassAnnotationTest {
 
 	@Rule
 	public InMemoryRule myRule = new InMemoryRule();
-	
+
 	@Test
 	public void oneBikerAndOneBicycleForThisBikerCreatedByEnvironment() {
 		List<Biker> bikers = BikerInMemorySupport.findAll();
 		assertThat(bikers.size(), is(1));
-		
+
 		Biker biker = bikers.get(0);
 		assertThat(biker.getGender(), is(Gender.MALE));
 		assertThat(biker.getName(), is("Lemão"));
-		
+
 		List<Bicycle> bicycles = BicycleInMemorySupport.findAll();
 		assertThat(bicycles.size(), is(1));
-		
+
 		Bicycle bicycle = bicycles.get(0);
 		assertThat(bicycle.getModelName(), is("S-WORKS EPIC 29"));
 		assertThat(bicycle.getSerialNumber(), is(165487L));
 		assertThat(bicycle.getOwner(), is(biker));
 	}
-	
+
 	@Test
 	@IgnoreEnvironment
 	public void nothingCreatedBecauseTheEnvironmentWasIgnored() {
 		assertTrue(BikerInMemorySupport.findAll().isEmpty());
 		assertTrue(BicycleInMemorySupport.findAll().isEmpty());
 	}
-	
+
 	@Test
 	@GivenEnvironment(TwoBikersOneMaleAnotherFemaleAndOneBicycleForMaleBiker.class)
 	public void twoBikersOneMaleAnotherFemaleAndOneBicycleForMaleBikerCreatedByEnvironment() {
 		List<Biker> bikers = BikerInMemorySupport.findAll();
 		assertThat(bikers.size(), is(2));
-		
+
 		Biker lemaoBiker = BikerInMemorySupport.findByName("Lemão");
 		assertThat(lemaoBiker.getGender(), is(Gender.MALE));
 		assertThat(lemaoBiker.getName(), is("Lemão"));
-		
+
 		Biker oliviaBiker = BikerInMemorySupport.findByName("Olivia");
 		assertThat(oliviaBiker.getGender(), is(Gender.FEMALE));
 		assertThat(oliviaBiker.getName(), is("Olivia"));
-		
+
 		List<Bicycle> bicycles = BicycleInMemorySupport.findAll();
 		assertThat(bicycles.size(), is(1));
-		
+
 		Bicycle bicycle = bicycles.get(0);
 		assertThat(bicycle.getModelName(), is("S-WORKS EPIC 29"));
 		assertThat(bicycle.getSerialNumber(), is(165487L));
 		assertThat(bicycle.getOwner(), is(lemaoBiker));
 	}
-	
+
 	@Test
-	@GivenEnvironment(value=BikerEnvironment.class, environmentName="twoBikersOneMaleAnotherFemale")
+	@GivenEnvironment(value = BikerEnvironment.class, environmentName = "twoBikersOneMaleAnotherFemale")
 	public void twoBikersOneMaleAnotherFemaleCreatedByEnvironment() {
 		List<Biker> bikers = BikerInMemorySupport.findAll();
 		assertThat(bikers.size(), is(2));
-		
+
 		Biker zeBiker = BikerInMemorySupport.findByName("Zé Grandão");
 		assertThat(zeBiker.getGender(), is(Gender.MALE));
 		assertThat(zeBiker.getName(), is("Zé Grandão"));
-		
+
 		Biker maricotinhaBiker = BikerInMemorySupport.findByName("Maria Maricotinha");
 		assertThat(maricotinhaBiker.getGender(), is(Gender.FEMALE));
 		assertThat(maricotinhaBiker.getName(), is("Maria Maricotinha"));
 	}
-	
+
 	@Test
-	@GivenEnvironment(value=BikerEnvironment.class, environmentName="twoBikersOneMaleAnotherFemaleWithBicycles")
+	@GivenEnvironment(value = BikerEnvironment.class, environmentName = "twoBikersOneMaleAnotherFemaleWithBicycles")
 	public void twoBikersOneMaleAnotherFemaleWithBicyclesCreatedByEnvironment() {
 		List<Biker> bikers = BikerInMemorySupport.findAll();
 		assertThat(bikers.size(), is(2));
-		
+
 		Biker zeBiker = BikerInMemorySupport.findByName("Zé Grandão");
 		assertThat(zeBiker.getGender(), is(Gender.MALE));
 		assertThat(zeBiker.getName(), is("Zé Grandão"));
-		
+
 		Biker maricotinhaBiker = BikerInMemorySupport.findByName("Maria Maricotinha");
 		assertThat(maricotinhaBiker.getGender(), is(Gender.FEMALE));
 		assertThat(maricotinhaBiker.getName(), is("Maria Maricotinha"));
-		
+
 		List<Bicycle> bicycles = BicycleInMemorySupport.findAll();
 		assertThat(bicycles.size(), is(2));
-		
+
 		Bicycle epicBicycle = BicycleInMemorySupport.findByModelName("S-WORKS EPIC 29");
 		assertThat(epicBicycle.getModelName(), is("S-WORKS EPIC 29"));
 		assertThat(epicBicycle.getSerialNumber(), is(165487L));
 		assertThat(epicBicycle.getOwner(), is(zeBiker));
-		
+
 		Bicycle allezBicycle = BicycleInMemorySupport.findByModelName("S-WORKS ALLEZ DI2");
 		assertThat(allezBicycle.getModelName(), is("S-WORKS ALLEZ DI2"));
 		assertThat(allezBicycle.getSerialNumber(), is(98657L));
 		assertThat(allezBicycle.getOwner(), is(maricotinhaBiker));
 	}
-	
+
 	@Test
-	@GivenEnvironment(value=BikerEnvironment.class, environmentName="twoBikersOneMaleAnotherFemale")
+	@GivenEnvironment(value = BikerEnvironment.class, environmentName = "twoBikersOneMaleAnotherFemale")
 	public void twoBikersOneMaleAnotherFemaleWithBicyclesCreatedByTest() {
 		List<Biker> bikers = BikerInMemorySupport.findAll();
 		assertThat(bikers.size(), is(2));
-		
+
 		Biker zeBiker = BikerInMemorySupport.findByName("Zé Grandão");
 		assertThat(zeBiker.getGender(), is(Gender.MALE));
 		assertThat(zeBiker.getName(), is("Zé Grandão"));
-		
+
 		Biker maricotinhaBiker = BikerInMemorySupport.findByName("Maria Maricotinha");
 		assertThat(maricotinhaBiker.getGender(), is(Gender.FEMALE));
 		assertThat(maricotinhaBiker.getName(), is("Maria Maricotinha"));
-		
+
 		assertTrue(BicycleInMemorySupport.findAll().isEmpty());
-		
-		Bicycle epicBicycle = oneBicycle()
-				.forBiker(zeBiker)
-				.withModelName("S-WORKS EPIC 29")
-				.withSerialNumber(165487L)
+
+		Bicycle epicBicycle = oneBicycle().forBiker(zeBiker).withModelName("S-WORKS EPIC 29").withSerialNumber(165487L)
 				.gimme();
 		BicycleInMemorySupport.persist(epicBicycle);
-		
-		Bicycle allezBicycle = oneBicycle()
-				.forBiker(maricotinhaBiker)
-				.withModelName("S-WORKS ALLEZ DI2")
-				.withSerialNumber(98657L)
-				.gimme();
+
+		Bicycle allezBicycle = oneBicycle().forBiker(maricotinhaBiker).withModelName("S-WORKS ALLEZ DI2")
+				.withSerialNumber(98657L).gimme();
 		BicycleInMemorySupport.persist(allezBicycle);
-		
+
 		List<Bicycle> bicycles = BicycleInMemorySupport.findAll();
 		assertThat(bicycles.size(), is(2));
-		
+
 		epicBicycle = BicycleInMemorySupport.findByModelName("S-WORKS EPIC 29");
 		assertThat(epicBicycle.getModelName(), is("S-WORKS EPIC 29"));
 		assertThat(epicBicycle.getSerialNumber(), is(165487L));
 		assertThat(epicBicycle.getOwner(), is(zeBiker));
-		
+
 		allezBicycle = BicycleInMemorySupport.findByModelName("S-WORKS ALLEZ DI2");
 		assertThat(allezBicycle.getModelName(), is("S-WORKS ALLEZ DI2"));
 		assertThat(allezBicycle.getSerialNumber(), is(98657L));
 		assertThat(allezBicycle.getOwner(), is(maricotinhaBiker));
+	}
+
+	@Test
+	@GivenEnvironments(environments = { @GivenEnvironment(TwoBicyclesEnvironment.class),
+			@GivenEnvironment(OneMaleBikerWithBicycleEnvironment.class),
+			@GivenEnvironment(OneFemaleBikerWithBicycleEnvironment.class) })
+	public void twoBikersWithBicyclesCreatedByTestMethodMultipleEnvironments() {
+		List<Biker> bikers = BikerInMemorySupport.findAll();
+		assertThat(bikers.size(), is(2));
+
+		Biker lemaoBiker = BikerInMemorySupport.findByName("Lemão");
+		assertThat(lemaoBiker.getGender(), is(Gender.MALE));
+		assertThat(lemaoBiker.getName(), is("Lemão"));
+
+		Biker mariaBiker = BikerInMemorySupport.findByName("Maria Maricotinha");
+		assertThat(mariaBiker.getGender(), is(Gender.FEMALE));
+		assertThat(mariaBiker.getName(), is("Maria Maricotinha"));
+
+		List<Bicycle> bicycles = BicycleInMemorySupport.findAll();
+		assertThat(bicycles.size(), is(2));
+
+		Bicycle bicycleBlue = BicycleInMemorySupport.findByModelName("S-WORKS EPIC 29 - BLUE");
+		assertThat(bicycleBlue.getModelName(), is("S-WORKS EPIC 29 - BLUE"));
+		assertThat(bicycleBlue.getSerialNumber(), is(165487L));
+		assertThat(bicycleBlue.getOwner(), is(lemaoBiker));
+
+		Bicycle bicyclePink = BicycleInMemorySupport.findByModelName("S-WORKS EPIC 29 - PINK");
+		assertThat(bicyclePink.getModelName(), is("S-WORKS EPIC 29 - PINK"));
+		assertThat(bicyclePink.getSerialNumber(), is(132423L));
+		assertThat(bicyclePink.getOwner(), is(mariaBiker));
+
 	}
 
 }
